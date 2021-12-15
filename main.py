ec31768ddf5b921f45d9c9ae16cca6166b7e58ec
camspec <69281585+camspec@users.noreply.github.com>
2021-12-14 20:01:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import pause
 import win32api
+from discord_webhook import DiscordWebhook
 from datetime import datetime, time, timedelta, timezone
+import os
 
 
 def utc_to_local(utc):
     return utc.replace(tzinfo=timezone.utc).astimezone(tz=None)
 
+
+if not os.path.isfile("./webhookurl.txt"):
+    url = input("Paste webhook url: ")
+    with open("webhookurl.txt", "w") as file:
+        file.write(url)
+    webhook = DiscordWebhook(url=url, content="CH Boss ends in 5 minutes, resets in 10 minutes.")
+else:
+    with open("webhookurl.txt") as file:
+        webhook = DiscordWebhook(url=file.readlines(), content="CH Boss ends in 5 minutes, resets in 10 minutes.")
+        print("*To change your webhook url, edit webhookurl.txt*")    
 
 print("Successfully running")
 while True:
@@ -16,4 +28,5 @@ while True:
     designated_time = time(hour=23, minute=50)  # 23:50 UTC. boss ends at 23:55 and resets at 00:00, no dst
     designated_date = datetime.combine(datetime.utcnow(), designated_time)
     pause.until(utc_to_local(designated_date))
+    response = webhook.execute()
     win32api.MessageBox(0, "CH Boss ends in 5 minutes, resets in 10 minutes.", "ATTENTION!", 0x00001000)
